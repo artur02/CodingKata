effd33b7b39aaf58b280d4a8181eb9a947b0a1c1
Artur Herczeg <artur.herczeg@gmail.com>
2012-09-20 16:19:07 +0200

    CalcStat

---
+++ CalcStats/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let arr = [|
+                6
+                9
+                15
+                -2
+                92
+                11
+            |]
+
+let calcstat (arr : int[]) =
+    printf "minimum value = %d\n" (Array.min arr)
+    printf "maximum value = %d\n" (Array.max arr)
+    printf "num of element = %d\n" (Array.length arr)
+    printf "avarage = %f\n" (Array.averageBy (fun elem -> float elem) arr)
+
+[<EntryPoint>]
+let main argv = 
+    calcstat arr
+    0 // return an integer exit code
