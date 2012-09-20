53d0815c693374c8f470a938abc418351b5b0846
Artur Herczeg <artur.herczeg@gmail.com>
2012-09-20 15:33:33 +0200

    FizzBuzz

---
+++ FizzBuzz/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let mod3 num = num%3=0
+let mod5 num = num%5=0
+
+let (|DivThreeFive|DivThree|DivFive|General|)  (number : int) = 
+    if mod3 number && mod5 number then DivThreeFive
+    elif mod3 number then DivThree 
+    elif mod5 number then DivFive
+    else General                                
+
+let fizzbuzz =
+    for i=1 to 100 do
+        match i with
+        | DivThree -> printf "Fizz"
+        | DivFive -> printf "Buzz"
+        | DivThreeFive -> printf "FizzBuzz"
+        | General -> printf "%d" i
+        printf "\n"
+
+[<EntryPoint>]
+let main argv = 
+    fizzbuzz
+    0 // return an integer exit code
